dcedc43f10193167faeca5836ef1413968874a54
katsu taka <fruit.cluster2kindly@gmail.com>
2014-12-09 18:15:48 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,10 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
 
     describe "Help page" do
       it "help page open" do
@@ -22,6 +26,10 @@ describe "StaticPages" do
       it "should have the content 'Help'" do
         visit '/static_pages/help'
         expect(page).to have_content('Help')
+      end
+      it "should have the title 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
       end
     end
 
@@ -34,6 +42,10 @@ describe "StaticPages" do
       it "should have the content 'About Us'" do
         visit '/static_pages/about'
         expect(page).to have_content('About Us')
+      end
+      it "should have the title 'About Us'" do
+        visit '/static_pages/about'
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
       end
     end
 
